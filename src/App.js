4166f5a6f02280fbcd1ebcd5ff43763ac1bc23d4
cxrvxxxx <brentempasisx3@gmail.com>
2023-09-29 15:05:28 +0800

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
+import React, { useEffect, useState, useRef } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const key = useRef(0);
+
+  const [singers, setSingers] = useState([
+    {
+      "id": 1,
+      "color": "red"
+    },
+    {
+      "id": 2,
+      "color": "orange"
+    },
+    {
+      "id": 3,
+      "color": "yellow"
+    },
+    {
+      "id": 4,
+      "color": "blue"
+    }
+  ]);
+
+  const [phrase, setPhrase] = useState("");
+  const [lyrics, setLyrics] = useState([]);
+
+  const { id } = useParams();
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setPhrase(e.target.value);
+  }
+
+  useEffect(() => {
+    setPhrase("");
+  }, [id]);
+
+  useEffect(() => {
+    if (phrase === "") return;
+
+    const updatedLyrics = [...lyrics];
+
+    if (updatedLyrics.length > 0) {
+      if (updatedLyrics[updatedLyrics.length - 1].singer.id == id) {
+
+        updatedLyrics[updatedLyrics.length - 1].value = phrase;
+      } else {
+        updatedLyrics.push({
+          "singer": singers[id - 1],
+          "value": phrase
+        });
+      }
+    } else {
+      updatedLyrics.push({
+        "singer": singers[id - 1],
+        "value": phrase
+      });
+    }
+
+    setLyrics(updatedLyrics);
+  }, [phrase]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="selection">
+        {singers.map((singer) => (
+          <Link key={key.current++} to={`/singers/${singer.id}`}>
+            <div
+              key={key.current++}
+              className={"button " + singer.color}
+
+            >{"Singer " + singer?.id}</div>
+          </Link>
+        ))}
+      </div>
+      <div className="form-container">
+        <input
+          type="text"
+          value={phrase}
+          className="input-lyrics"
+          id="input-lyrics"
+          placeholder="Start typing..."
+          onChange={handleChange}
+        />
+      </div>
+      <div className="lyrics-container">
+        <div className="lyrics">
+          {lyrics.map((lyric) => (
+            <div className={"phrase " + lyric.singer.color}>{lyric.value}</div>
+          ))}
+        </div>
+      </div>
+    </div >
   );
 }
 
